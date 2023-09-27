ac44c4d9d3eef18ffa537cb60fb3b37102831cb9
harleigh <harleigh.marsh@gmail.com>
2023-09-27 12:46:56 -0700

    clear-list button implemented

--- src/Components/PackingList.js
+++ src/Components/PackingList.js
@@ -3,14 +3,23 @@ import { PackingItem } from "./PackingItem";
 // display the list items here
 //
 export function PackingList({ itemsToPack, setList }) {
+  //
   const buildListItems = () => {
     return itemsToPack.map((todo) => {
       return <PackingItem toPack={todo} setList={setList} key={todo.id} />;
     });
   };
+  const handleClearList = () => {
+    const clearList = window.confirm("Clear Packing List?");
+    if (clearList) {
+      setList([]);
+    }
+  };
+  //
   return (
     <div className="list">
       <ul>{buildListItems()}</ul>
+      <button onClick={handleClearList}>Clear List</button>
     </div>
   );
 }
